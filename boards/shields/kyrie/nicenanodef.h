59e042b6f4036f3903c666bf54ba79806b4bb959
vermilion00 <106870608+vermilion00@users.noreply.github.com>
2024-05-19 17:44:01 +0200

    Fixed define names

--- boards/shields/kyrie/nicenanodef.h
+++ boards/shields/kyrie/nicenanodef.h
@@ -1,24 +1,24 @@
 //Left side pins
-#define 006 1
-#define 008 0
-#define 017 2
-#define 020 3
-#define 022 4
-#define 024 5
-#define 100 6
-#define 011 7
-#define 104 8
-#define 106 9
+#define P006 1
+#define P008 0
+#define P017 2
+#define P020 3
+#define P022 4
+#define P024 5
+#define P100 6
+#define P011 7
+#define P104 8
+#define P106 9
 
 //Right side pins
-#define 031 21
-#define 029 20
-#define 002 19
-#define 115 18
-#define 113 15
-#define 111 14
-#define 010 16
-#define 009 10
+#define P031 21
+#define P029 20
+#define P002 19
+#define P115 18
+#define P113 15
+#define P111 14
+#define P010 16
+#define P009 10
 
 //Middle pins
 //referenced by &gpio1 7 = Pin 107 etc

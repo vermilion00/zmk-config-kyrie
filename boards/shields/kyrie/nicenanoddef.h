838ba4d4059ed3cc4bb1a335649cb90f08b7dbb8
vermilion00 <106870608+vermilion00@users.noreply.github.com>
2024-05-19 17:28:03 +0200

    Changed to Kyrie config

---
+++ boards/shields/kyrie/nicenanoddef.h
@@ -0,0 +1,24 @@
+//Left side pins
+#define 006 1
+#define 008 0
+#define 017 2
+#define 020 3
+#define 022 4
+#define 024 5
+#define 100 6
+#define 011 7
+#define 104 8
+#define 106 9
+
+//Right side pins
+#define 031 21
+#define 029 20
+#define 002 19
+#define 115 18
+#define 113 15
+#define 111 14
+#define 010 16
+#define 009 10
+
+//Middle pins
+//referenced by &gpio1 7 = Pin 107 etc
